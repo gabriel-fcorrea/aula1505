3c4457ddb2932f353c8c0dedc89e7078c8748258
gabriel-fcorrea <gabrecorrea@gmail.com>
2025-05-15 20:25:17 -0300

    soma de vetores

--- main.cpp
+++ main.cpp
@@ -4,17 +4,22 @@ using namespace std;
 int main(){
 	
 	int n;
-	cout << "Digite o tamanho do vetor: ";
+	cout << "Digite a quantidade de valores para somar: ";
 	cin >> n;
 	int vect[n];
+	int vectAux[n];
+	int vectSum[n];
 	
 	for (int i = 0; i < n; i++){
 		cout << "Digite um valor: ";
 		cin >> vect[i]; 
+		cout << "Digite outro valor: ";
+		cin >> vectAux[i]; 
+		vectSum[i] = vect[i] + vectAux[i];
 	}
 	
 	for (int i = 0; i < n; i++){
-		cout << "Valor na posicao [" << i << "]: " <<vect[i] << endl;
+		cout << vect[i] << " + " << vectAux[i] << " = "<< vectSum[i] << endl;
 	}
 	
 	return 0;
